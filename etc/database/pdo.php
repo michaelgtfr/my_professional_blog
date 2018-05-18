9f4f1fe4d2fd7d4a71b9f12cc15a4a444c28a493
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2018-05-18 16:27:35 +0200

    fix(PDO): fix on PDO call for configuration

--- etc/database/pdo.php
+++ etc/database/pdo.php
@@ -7,9 +7,7 @@ require ('../../config/pdo.php');
 
 function pdo()
 {
-
-
-	$db = new PDO('mysql:host=$host;dbname=$dbname;charset=$charset', '$login', '$password');
-	return $db;
-
+    $config = require __DIR__.'./../../config/pdo.php';
+	
+    return new PDO('mysql:host='.$config['host'].';dbname='.$config['dbname'].';charset='.$config['charset'].', $config['username'], $config['password');
 }
