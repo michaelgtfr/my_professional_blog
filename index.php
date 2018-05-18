839200032ebdc98e1a600dd89c457798bac77817
mickdu62 <mickdu62200@gmail.com>
2018-05-18 12:50:30 +0200

    creation router containing the error handler

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+	//Router 
+
+try
+{
+
+}
+
+catch(Exception $e)
+{
+	echo 'Erreur: ' . $e -> getMessage();
+
+	// modified the part "catch" et create a file that will display an error message 
+}
