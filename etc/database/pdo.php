a70229ccba01da310ad5a36bf10daed5095aec61
mickdu62 <mickdu62200@gmail.com>
2018-05-21 11:36:28 +0200

    Modification the write of the pdo function

--- etc/database/pdo.php
+++ etc/database/pdo.php
@@ -9,6 +9,6 @@ function pdo()
 	$config = require __DIR__ . '../..config/pdo.php';
 
 
-	return new PDO('mysql:host='. $config['host']. ';dbname='. $config['dbname'] . ';charset='. $config['charset'].' , $config['login'], $config['password']);
+	return new PDO('mysql:host='. $config['host']. ';dbname='. $config['dbname'] . ';charset='. $config['charset'] .','. $config['username'] .','. $config['password'] .);
 
 }
