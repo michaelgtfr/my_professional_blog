4e835eaef4e29b0971c66fa95693b8696e38fca1
mickdu62 <mickdu62200@gmail.com>
2018-06-22 14:29:55 +0200

    file name controler.php and content edit

---
+++ src/controler/home.php
@@ -0,0 +1,20 @@
+<?php
+
+//require __DIR__.'./../src/domaine/repository/requete.php';
+
+require __DIR__.'./../../etc/templateLoader.php';
+
+function home(){
+	//$lastThreeAddition = lastThreeAddition();
+
+	//if(isset($lastThreeAddition))
+	//{
+
+	loadTemplate('home.php');
+
+	//}
+	//else
+	//{
+		//throw new Exception('Desolate item recovery problem');
+	//}	
+}
