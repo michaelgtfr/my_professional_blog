8efa7fba7092a5c9677dbb5b44a30a98e91dcd1c
mickdu62 <mickdu62200@gmail.com>
2018-07-06 15:44:43 +0200

    no modification

---
+++ etc/router.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__.'./actionResolver.php';
+
+function handleRequest(array $request) {
+  $routes = require __DIR__.'./../config/routes.php';
+
+  foreach ($routes as $key => $value){
+    switch ($value) {
+      case $value['path'] === $request['REQUEST_URI'] && in_array($_SERVER['REQUEST_METHOD'], $value['method']);
+          resolveAction($value['action']);
+          break;
+    }
+  }
+}
+
+function catchParams(string $params, string $path) {
+  $param = preg_match($params, $path, $result);
+  var_dump($param);
+}
