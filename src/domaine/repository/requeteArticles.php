1495b2bb72d9499ad1df94fbf1648309e938e577
mickdu62 <mickdu62200@gmail.com>
2018-07-12 17:16:03 +0200

    modification of bug of the requete

--- src/domaine/repository/requeteArticles.php
+++ src/domaine/repository/requeteArticles.php
@@ -2,29 +2,34 @@
 
 require __DIR__.'./../../../etc/database/pdo.php';
 
-function listingOfArticles(){
+function listingOfArticles($firstEnter, $messagesByPage)
+{
 
 	$db = pdo();
 
 	if(isset($db)){
 
-		$req = $db->query('SELECT 	blog_posts.id AS id,
+		$req = $db->prepare('SELECT 	blog_posts.id AS id,
 									blog_posts.title AS title,
 									blog_posts.chapo AS chapo, 
 									blog_posts.validate_blog_post AS validate,
 									blog_posts.autor AS author,
-									picture.blog_post_id AS id_picture, 
+									picture.blog_posts_id AS id_picture, 
 									picture.name AS name_picture, 
 									picture.extention AS extention_picture, 
 									picture.description AS description_picture
 			FROM blog_posts 
 			INNER JOIN picture
-			ON blog_posts.id= picture.blog_post_id
-			WHERE validate_blog_post = TRUE ORDER BY DESC LIMIT'. $premiereEntree.', '.$messagesParPage.'');
+			ON blog_posts.id = picture.blog_posts_id
+			WHERE validate_blog_post = true ORDER BY id DESC LIMIT :firstEnter, :messagesByPage');
+
+		$req->bindParam(':firstEnter', $firstEnter, PDO::PARAM_INT);
+		$req->bindParam(':messagesByPage', $messagesByPage, PDO::PARAM_INT);
+		$req->execute();
 
 		return $req;
-	}
-	else{
-		echo ' probleme requeteArticles'; //francais
+
+	} else {
+		echo ' error requete'; 
 	}
 }
