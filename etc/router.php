a758501125430080d4658ea23de0414174a3617a
mickdu62 <mickdu62200@gmail.com>
2018-07-12 17:07:23 +0200

    modification of the router, modification of the bug involved cathParam

--- etc/router.php
+++ etc/router.php
@@ -5,16 +5,32 @@ require __DIR__.'./actionResolver.php';
 function handleRequest(array $request) {
   $routes = require __DIR__.'./../config/routes.php';
 
-  foreach ($routes as $key => $value){
+  foreach ($routes as $key => $value) {
+
     switch ($value) {
-      case $value['path'] === $request['REQUEST_URI'] && in_array($_SERVER['REQUEST_METHOD'], $value['method']);
-          resolveAction($value['action']);
+      case in_array($_SERVER['REQUEST_METHOD'], $value['method']):
+          $params = catchParams($value['params'] ?? [], $request['REQUEST_URI'], $value['path']);
+          if ($value['path'] === $request['REQUEST_URI'] && in_array($_SERVER['REQUEST_METHOD'], $value['method'])) {
+            resolveAction($value['action'], $params  ?? []);
+          }
           break;
     }
   }
 }
 
-function catchParams(string $params, string $path) {
-  $param = preg_match($params, $path, $result);
-  var_dump($param);
+function catchParams(array $params, string $path, &$routePath) {
+
+  $results = [];
+
+  foreach ($params as $key => $regex) {
+    preg_match('#'.$regex.'#', $path, $result);
+    if (is_null($result)) {
+
+      return;
+    }
+    elseif (!empty($result)) {
+      $routePath = strtr($routePath, ['{'.$key.'}' => $result[0]]);
+      return $result;
+    }
+  } 
 }
