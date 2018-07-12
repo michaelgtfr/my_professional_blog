77b451690829f9984dba316ac7706ea5daf374cc
mickdu62 <mickdu62200@gmail.com>
2018-07-12 17:05:38 +0200

    no modification

--- etc/actionResolver.php
+++ etc/actionResolver.php
@@ -3,7 +3,7 @@
 require __DIR__.'./../src/controler/home.php';
 require __DIR__.'./../src/controler/listArticles.php';
 
-function resolveAction(string $action, array $params = []) {
+function resolveAction(string $action, array $params = [])
+{
   call_user_func($action, $params);
-  
 }
