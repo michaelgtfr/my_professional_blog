ce8d739cd512ef62270848b50964d15e9d99faae
mickdu62 <mickdu62200@gmail.com>
2018-07-12 17:18:14 +0200

    edit link to function bug and set variables in english

--- src/fonction/pagination.php
+++ src/fonction/pagination.php
@@ -1,41 +1,38 @@
 <?php
 
-function pagination($table, $params, $req){
+function pagination($table, $params){
 
+$messagesByPage=5; 
 
-$messagesParPage=5; //Nous allons afficher 5 messages par page.
- 
-//Une connexion SQL doit être ouverte avant cette ligne...
 $db = pdo();
 
-$retour_total= $db->prepare('SELECT COUNT(*) AS total FROM'. $table .''); //Nous récupérons le contenu de la requête dans $retour_total
-$donnees_total= $retour_total->fetch(); //On range retour sous la forme d'un tableau.
-$total=$donnees_total['total']; //On récupère le total pour le placer dans la variable $total.
+$totalReturn= $db->query('SELECT COUNT(*) AS total FROM '. $table .''); 
+
+$totalData= $totalReturn->fetch();
+$total=$totalData['total']; 
+
+$numberOfPages=ceil($total/$messagesByPage);
+
  
-//Nous allons maintenant compter le nombre de pages.
-$nombreDePages=ceil($total/$messagesParPage);
- 
-if(isset($_GET['page'])) // Si la variable $_GET['page'] existe...
-{
-     $pageActuelle=intval($_GET['page']);
- 
-     if($pageActuelle>$nombreDePages) // Si la valeur de $pageActuelle (le numéro de la page) est plus grande que $nombreDePages...
-     {
-          $pageActuelle=$nombreDePages;
+if(isset($params)) {
+     $currentPage=intval($params);
+
+    if($currentPage>$numberOfPages) {
+     
+        $currentPage=$numberOfPages;
+
      }
-}
-else // Sinon
-{
-     $pageActuelle=1; // La page actuelle est la n°1    
-}
- 
-$premiereEntree=($pageActuelle-1)*$messagesParPage; // On calcul la première entrée à lire
- 
-// La requête sql pour récupérer les messages de la page actuelle.
-$retour_messages= $req;
+} else {
+    $currentPage=1;
 
-$retour_messages = array($retour_messages, $i, $nombreDePages, $pageActuelle);
+}
 
-return $retour_messages;
+$firstEnter=($currentPage-1)*$messagesByPage; 
+
+$reqMessages= listingOfArticles($firstEnter, $messagesByPage);
+
+$returnMessages = array($reqMessages, $numberOfPages, $currentPage);
+
+return $returnMessages;
 
 }
