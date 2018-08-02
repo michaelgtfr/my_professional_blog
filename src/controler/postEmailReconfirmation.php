a306c39a522f8d0be0b6446ef0cc7f6a66272eff
mickdu62 <mickdu62200@gmail.com>
2018-08-02 15:52:42 +0200

    File processing request for re-sending of confirmation email

---
+++ src/controler/postEmailReconfirmation.php
@@ -0,0 +1,41 @@
+<?php 
+
+function postEmailReconfirmation()
+{
+	$email = htmlspecialchars($_POST['email']);
+
+    $req = confirmation($email);
+    $result = $req->fetch();
+
+    $key = $result['confirmation_key'];
+	$confirmation = $result['confirmation'];
+
+	if($confirmation == 1) {
+    	$message = 'votre compte est déjà confirmé, veuillez attendre qu\'un administrateur valide votre compte';
+    	loadTemplate('message.php', $message);
+	} elseif(!empty($key)) {
+        $to = $email; 
+
+        $subject = 'Confirmation de votre compte';
+ 
+        $message = 'bienvenue sur mon blog professionnel,
+
+        Pour activer votre compte, veuillez cliquer sur le lien ci dessous ou copier/coller dans votre navigateur internet.
+
+        http://projetCinq/index.php/accountConfirmation?activation='.urlencode($email). '&cle='.urlencode($key).'
+
+
+
+        --------------
+        Ceci est un email automatique, Merci de ne pas y répondre';
+
+        sendEmail($to, $subject, $message);
+
+        loadTemplate('postRegistration.php', $email);
+	} else {
+		$message = "desoler, le message ne peut pas être ré-envoyé. Contacter un administrateur du site via le formulaire de contact sur la page d'accueil";
+		loadTemplate('message.php', $message);
+	}
+
+
+}
