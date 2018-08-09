2b355a23ef9a971e7d854a8fe082e61a48a0abd8
mickdu62 <mickdu62200@gmail.com>
2018-08-09 15:22:17 +0200

    File modelisant The data send by the controller commentManagement.php

---
+++ templates/commentManagement.php
@@ -0,0 +1,33 @@
+<?php $title = 'item detail' ?>
+<?php $style ='<link rel="stylesheet" href="http://projetcinq/css/commentManagement.css" />' ?>
+
+<!--body-->
+<?php ob_start(); ?>
+
+<section class="tableau">                                            <!--francais-->
+    <?php
+        $message = $returnMessages[0];
+        $req = $returnMessages[1];
+    ?>
+	<h3>Tableau de validation des commentaires:</h3>
+    <p><?= $message ?></p>
+	<?php
+	    while ($data = $req->fetch()) {
+        ?>
+            <p><?= $data['author'] ?><p>
+            <p><?= $data['date_create'] ?></p>
+            <p><?= $data['content'] ?></p>
+            <p><a href="http://projetcinq/index.php/validatecomment/<?= $data['id'] ?>">Valider le commentaire</a></p>
+            <p><a href="http://projetcinq/index.php/deletecomment/<?= $data['id'] ?>">Refuser le commentaire</a></p>
+        <?php
+        }
+        
+	    $req->closeCursor();
+
+	?>
+
+
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require __DIR__.'/template.php'; ?>
