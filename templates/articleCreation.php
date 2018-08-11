cdb6939a8fe0079ceb808151074f2a388c62f3fa
mickdu62 <mickdu62200@gmail.com>
2018-08-11 13:17:44 +0200

    file containing an article creation form

---
+++ templates/articleCreation.php
@@ -0,0 +1,24 @@
+<?php $title = 'item detail' ?>
+<?php $style ='<link rel="stylesheet" href="http://projetcinq/css/articleDetail.css" />' ?>
+
+<!--body-->
+<?php ob_start(); ?>
+<section class="formulaire">
+	<h3>Formulaire de création d'article</h3>
+	<form action="/index.php/postarticlecreation" method="post" enctype="multipart/form-data">
+	  <div>
+	  	<input type="hidden" name="id" value="<?= $_SESSION['id'] ?>" />
+	    <label for="title">Titre:</label><input type="text" name="title" id="title" />
+	    <label for="chapo">Chapo:</label><input type="text" name="chapo" id="chapo" />
+	    <label for="content">Content</label><textarea name="content" id="content">Ecrivez ici votre message</textarea>
+	    <input type="hidden" name="MAX_FILE_SIZE" value="2097152" />
+		<label for="photo">Photo/Logo (max 2Mo, JPG, PNG) : </label><input type="file" name="photo" id="photo" />
+		<label for="description">Description</label><input type="text" name="description" id="description" />
+		<button type="submit">Valider</button>
+
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require __DIR__.'/template.php'; ?>
+
+}
