aaf40fd7821d0a2aa805b9c4fae4902f3a05f3f5
mickdu62 <mickdu62200@gmail.com>
2018-08-12 11:36:28 +0200

    File for setting up elements on the templates

---
+++ templates/deletedArticle.php
@@ -0,0 +1,16 @@
+<?php $title = 'item detail' ?>
+<?php $style ='<link rel="stylesheet" href="http://projetcinq/css/articleDetail.css" />' ?>
+
+<!--body-->
+<?php ob_start(); ?>
+<section class="confirmation">
+    <p>êtes vous sur de vouloir éffacer cette article</p>
+    <a href="http://projetcinq/index.php/articledetail/<?= $returnMessages ?>">Annuler</a>
+    <a href="http://projetcinq/index.php/postdeletedarticle/<?= $returnMessages ?>">Supprimer</a>
+</section>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require __DIR__.'/template.php'; ?>
+
+}
