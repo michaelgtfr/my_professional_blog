ef426bded89555361ad1a86cddadfd854232bace
mickdu62 <mickdu62200@gmail.com>
2018-09-07 13:50:20 +0200

    Recovered configuration in file PDO. PHP Config folder and enregistrevdans a variable ' settings '

---
+++ etc/Config.php
@@ -0,0 +1,20 @@
+<?php
+
+final class Config 
+{
+	private $settings = [];
+
+	public function loadConfigFromFile($fileName)
+	{
+		$this->settings = require_once dirname(__DIR__) .sprintf('/config/%s', $fileName);
+	}
+
+    public function get($key)
+    {
+    	if (!isset($this->settings[$key])) {
+            return null;
+    	}
+
+    	return $this->settings[$key];
+    }
+}
