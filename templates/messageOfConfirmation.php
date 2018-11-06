77cac4dd2b0b967c3a9f32c838b5f8ebde76032a
mickdu62 <mickdu62200@gmail.com>
2018-11-06 12:11:47 +0100

    registration message send to the user

---
+++ templates/messageOfConfirmation.php
@@ -0,0 +1,10 @@
+<p>Bienvenue sur mon blog professionnel,
+
+            Pour confirmer votre compte, veuillez cliquer sur le lien ci dessous ou copier/coller dans votre navigateur internet.
+
+            http://projetCinq/index.php/accountConfirmation?activation='<?= urlencode($email). '&cle='.urlencode($key); ?>'
+
+
+
+            --------------
+            Ceci est un email automatique, Merci de ne pas y répondre'</p>
