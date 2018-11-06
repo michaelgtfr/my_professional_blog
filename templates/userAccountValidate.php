225b40686cc1865d309479fdb6b68b37b79c36ac
mickdu62 <mickdu62200@gmail.com>
2018-11-06 12:15:52 +0100

    registration validation message send to the user

---
+++ templates/userAccountValidate.php
@@ -0,0 +1,8 @@
+<p>Félicitation! votre compte a été valider,
+
+        vous pouvez dès à présent vous connectez a votre compte utilisateur en cliquant ou en copiant l\'url dans votre navigateur <a href='http://projetcinq/'>http://projetcinq/</a> .
+
+
+        --------------
+        Ceci est un email automatique, Merci de ne pas y répondre</p>
+        
