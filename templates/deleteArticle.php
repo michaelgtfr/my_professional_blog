e96a41cdcbdfd35957eac1ad754b62200e353b76
mickdu62 <mickdu62200@gmail.com>
2018-11-06 12:09:54 +0100

    contains the message of refusal of an article

---
+++ templates/deleteArticle.php
@@ -0,0 +1,3 @@
+<p>Désoler votre message a été refusé. Pour plus de reponse sur ce refus vous pouvez dès a présent envoyer un message via le formualire de contact.
+    	--------------
+    	Ceci est un email automatique, Merci de ne pas y répondre'</p>
