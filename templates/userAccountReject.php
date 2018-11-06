85104b5723ad6bf001e3c7a28eb648c7a151a2bf
mickdu62 <mickdu62200@gmail.com>
2018-11-06 12:14:48 +0100

    rejection message send to the user

---
+++ templates/userAccountReject.php
@@ -0,0 +1,8 @@
+<p>Désoler! votre compte a été refuser,
+
+        vous pouvez n\'est en moins continuer à regarder et commenter les articles en cliquant ou en copiant l\'url dans votre navigateur <a href ='http://projetcinq/'>http://projetcinq/</a>.
+
+
+        --------------
+        Ceci est un email automatique, Merci de ne pas y répondre</p>
+        
