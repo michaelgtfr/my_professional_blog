47515b4fa08026e71e0cc4f1f245d0fdda80e2a9
mickdu62 <mickdu62200@gmail.com>
2018-11-06 12:17:17 +0100

    validation message of an article

---
+++ templates/validateArticle.php
@@ -0,0 +1,5 @@
+<p>Félicitation votre message a été valider, vous pouvez dès à présent voir votre article sur le blog.
+
+	--------------
+    Ceci est un email automatique, Merci de ne pas y répondre</p>
+    
